17da5563020a08ced1f36691c0244a85373d06db
Dhinakaran <dhina15003@gmail.com>
2024-03-12 12:12:03 +0530

    newly commited

--- src/assignment2/util.py
+++ src/assignment2/util.py
@@ -1,5 +1,5 @@
 def second_largest():
-    n = 6
-    array=[1,23,5,6,84,43]
+    n =int(input())
+    array=map(int, input().split())
     #array = map(int, input().split())
     return sorted(set(array))[-2]

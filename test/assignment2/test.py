ef768070f980b206d15c16b04cf71717cc38d5d6
Dhinakaran <dhina15003@gmail.com>
2024-03-12 12:09:35 +0530

    commited 3 testcase

--- test/assignment2/test.py
+++ test/assignment2/test.py
@@ -5,6 +5,13 @@ from src.assignment2.util import second_largest
 class MyTestCase(unittest.TestCase):
     def test_second_largest(self):
         answer=second_largest()
+        self.assertEqual(answer, 41)
+
+    def test_second_largest1(self):
+            answer = second_largest()
+            self.assertEqual(answer, 42)
+    def test_second_largest2(self):
+        answer=second_largest()
         self.assertEqual(answer, 43)
 
         # add assertion here

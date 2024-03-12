1018ce093b3b066cc3ecf1d55c48a9f892c64a3a
Dhinakaran <dhina15003@gmail.com>
2024-03-12 11:15:57 +0530

    3 test cases completed

--- test/assignment1/test.py
+++ test/assignment1/test.py
@@ -5,9 +5,22 @@ from src.assignment1.util import cal_avg
 class TestCalAvg(unittest.TestCase):
     def test_cal_avg(self):
         n=3
-        student_data=['harish 2.5 2.5 4','hari 2.5 3.5 3','lokesh 35 34 76']
-        query_name='hari'
-        self.assertEqual(cal_avg(n,student_data,query_name),'3.00')  # add assertion here
+        student_data=['dhina 1 2 3','deepak 1 4 7','deepa 5 7 2']
+        query_name='dhina'
+        self.assertEqual(cal_avg(n,student_data,query_name),'2.00')
+
+    def test1_cal_avg(self):
+        n=3
+        student_data=['a 4 6 8 ','b 4 4 4','c 5 5 5']
+        query_name='c'
+        self.assertEqual(cal_avg(n,student_data,query_name), '5.00')
+
+    def test2_cal_avg(self):
+        n=3
+        student_data=['e 1 1 1 ','f 10 10 10','g 20 20 20']
+        query_name='g'
+        self.assertEqual(cal_avg(n,student_data,query_name), '20.00')
+
 
 
 if __name__ == '__main__':
